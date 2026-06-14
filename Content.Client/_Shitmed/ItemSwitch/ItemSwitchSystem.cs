8d0e559c482118cd69a23d25b804340271b6800f
- <noel291205@gmail.com>
2026-06-14 15:34:18 +0200

    Added DS gloves and had to swith the itemswitch from starlight with the one from shitmed

--- Content.Client/_Shitmed/ItemSwitch/ItemSwitchSystem.cs
+++ Content.Client/_Shitmed/ItemSwitch/ItemSwitchSystem.cs
@@ -1,13 +1,16 @@
-﻿using Content.Shared.Starlight.ItemSwitch;
-using Content.Shared.Interaction.Events;
-using Content.Shared.Interaction;
-using Content.Shared.Item;
-using Content.Shared.Item.ItemToggle.Components;
-using Content.Shared.Toggleable;
-using Content.Shared.Verbs;
+// SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 Piras314 <p1r4s@proton.me>
+// SPDX-FileCopyrightText: 2025 gluesniffler <159397573+gluesniffler@users.noreply.github.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+using Content.Client.Items;
+using Content.Shared._Shitmed.ItemSwitch;
+using Content.Shared._Shitmed.ItemSwitch.Components;
+using Content.Client._Shitmed.ItemSwitch.UI;
 using Robust.Client.GameObjects;
 
-namespace Content.Client._Starlight.ItemSwitch;
+namespace Content.Client._Shitmed.ItemSwitch;
 
 public sealed class ItemSwitchSystem : SharedItemSwitchSystem
 {
@@ -15,6 +18,7 @@ public sealed class ItemSwitchSystem : SharedItemSwitchSystem
     {
         base.Initialize();
 
+        Subs.ItemStatus<ItemSwitchComponent>(ent => new ItemSwitchStatusControl(ent));
         SubscribeLocalEvent<ItemSwitchComponent, AfterAutoHandleStateEvent>(OnChanged);
     }
 
